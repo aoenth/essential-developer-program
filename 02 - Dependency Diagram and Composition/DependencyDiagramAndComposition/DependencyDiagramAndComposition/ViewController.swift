7072fe564b27e2da2ed52ddab22651fc4fa680f0
Kevin Peng <peng.kevin@me.com>
2021-08-19 23:01:31 -0400

    Update Dependency Diagrams and Composition

--- 02 - Dependency Diagram and Composition/DependencyDiagramAndComposition/DependencyDiagramAndComposition/ViewController.swift
+++ 02 - Dependency Diagram and Composition/DependencyDiagramAndComposition/DependencyDiagramAndComposition/ViewController.swift
@@ -1,50 +1,57 @@
 import UIKit
 
+protocol FeedLoader {
+    func loadFeed(completion: @escaping ([String]) -> Void)
+}
+
 struct Reachability {
     static let networkAvailable = false
 }
 
-class RemoteFeedLoaderWithLocalFallback: FeedLoader {
-    let localFeedLoader: LocalFeedLoader
-    let remoteFeedLoader: RemoteFeedLoader
-    init(localFeedLoader: LocalFeedLoader, remoteFeedLoader: RemoteFeedLoader) {
-        self.localFeedLoader = localFeedLoader
-        self.remoteFeedLoader = remoteFeedLoader
-    }
-
-    func loadFeed(completion: (FeedItem) -> ()) {
-        let load = Reachability.networkAvailable ? remoteFeedLoader.loadFeed : localFeedLoader.loadFeed
-        load(completion)
-    }
-}
-
-
-class LocalFeedLoader: FeedLoader {
-    func loadFeed(completion: (FeedItem) -> ()) {}
-}
-
-class RemoteFeedLoader: FeedLoader {
-    func loadFeed(completion: (FeedItem) -> ()) {}
-}
-
-struct FeedItem {}
-
-protocol FeedLoader {
-    func loadFeed(completion: (FeedItem) -> Void)
-}
-
 class FeedViewController: UIViewController {
-    var feedLoader: FeedLoader!
+    var loader: FeedLoader!
 
-    convenience init(feedLoader: FeedLoader) {
+    convenience init(loadFeed: FeedLoader) {
         self.init()
-        self.feedLoader = feedLoader
+        self.loader = loadFeed
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        feedLoader.loadFeed { feedItem in
 
+        loader.loadFeed { loadedItems in
+            // update UI
         }
     }
 }
+
+class RemoteFeedLoader: FeedLoader {
+    func loadFeed(completion: @escaping ([String]) -> Void) {
+        //
+    }
+}
+
+class LocalFeedLoader: FeedLoader {
+    func loadFeed(completion: @escaping ([String]) -> Void) {
+        //
+    }
+}
+
+class RemoteWithLocalFallbackFeedService: FeedLoader {
+    let remote: RemoteFeedLoader
+    let local: LocalFeedLoader
+
+    init(remote: RemoteFeedLoader, local: LocalFeedLoader) {
+        self.remote = remote
+        self.local = local
+    }
+
+    func loadFeed(completion: @escaping ([String]) -> Void) {
+        let load = Reachability.networkAvailable ? remote.loadFeed : local.loadFeed
+        load(completion)
+    }
+}
+
+let vc = FeedViewController(loadFeed: RemoteFeedLoader())
+let vc2 = FeedViewController(loadFeed: LocalFeedLoader())
+let vc3 = FeedViewController(loadFeed: RemoteWithLocalFallbackFeedService(remote: RemoteFeedLoader(), local: LocalFeedLoader()))
