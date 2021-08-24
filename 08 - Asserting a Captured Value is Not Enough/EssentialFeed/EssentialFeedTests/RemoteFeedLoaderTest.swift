c52b9e57de4ab7c6ecca0673220791fb9bb54ff6
Kevin Peng <peng.kevin@me.com>
2021-08-24 07:20:51 -0400

    Move `RemoteFeedLoader` to production

---
+++ 08 - Asserting a Captured Value is Not Enough/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -0,0 +1,42 @@
+//
+//  RemoteFeedLoaderTest.swift
+//  RemoteFeedLoaderTest
+//
+//  Created by Kevin Peng on 2021-08-19.
+//
+
+import XCTest
+import EssentialFeed
+
+class RemoteFeedLoaderTest: XCTestCase {
+
+    func test_init_doesNotRequestDataFromURL() {
+        let (_, client) = makeSUT()
+        XCTAssertNil(client.requestedURL)
+    }
+
+    func test_load_requestDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+
+        sut.load()
+
+        XCTAssertEqual(client.requestedURL, url)
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT(url: URL = URL(string: "https://a-given-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+
+        func get(from url: URL) {
+            requestedURL = url
+        }
+    }
+}
