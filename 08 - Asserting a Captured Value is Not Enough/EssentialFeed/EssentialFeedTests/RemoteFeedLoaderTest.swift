0f2c7673c822f978fb2ef3f34f80f3e4f06376d6
Kevin Peng <peng.kevin@me.com>
2021-08-24 07:26:44 -0400

    Simplify spy

--- 08 - Asserting a Captured Value is Not Enough/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ 08 - Asserting a Captured Value is Not Enough/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -12,7 +12,7 @@ class RemoteFeedLoaderTest: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
-        XCTAssertNil(client.requestedURL)
+        XCTAssertTrue(client.requestedURLs.isEmpty)
     }
 
     func test_load_requestsDataFromURL() {
@@ -21,7 +21,7 @@ class RemoteFeedLoaderTest: XCTestCase {
 
         sut.load()
 
-        XCTAssertEqual(client.requestedURL, url)
+        XCTAssertEqual(client.requestedURLs, [url])
     }
 
     func test_loadTwice_requestsDataFromURLTwice() {
@@ -43,11 +43,9 @@ class RemoteFeedLoaderTest: XCTestCase {
     }
 
     private class HTTPClientSpy: HTTPClient {
-        var requestedURL: URL?
         var requestedURLs = [URL]()
 
         func get(from url: URL) {
-            requestedURL = url
             requestedURLs.append(url)
         }
     }
