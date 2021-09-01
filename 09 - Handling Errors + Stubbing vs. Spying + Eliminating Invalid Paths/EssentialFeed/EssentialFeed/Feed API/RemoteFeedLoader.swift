4ff0f667bf51c39f2f8366b9aec3fc5f34c71e1e
Kevin Peng <peng.kevin@me.com>
2021-08-31 21:12:44 -0400

    Episode 9 Handling Errors + Stubbing vs. Spying + Eliminating Invalid Paths

---
+++ 09 - Handling Errors + Stubbing vs. Spying + Eliminating Invalid Paths/EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,33 @@
+//
+//  RemoteFeedLoader.swift
+//  RemoteFeedLoader
+//
+//  Created by Kevin Peng on 2021-08-24.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (Error) -> Void)
+}
+
+public final class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+
+    public enum Error: Swift.Error {
+        case connectivity
+    }
+
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+
+    public func load(completion: @escaping (Error) -> Void = { _ in }) {
+        client.get(from: url) { error in
+            completion(.connectivity)
+        }
+    }
+}
+
---
+++ 09 - Handling Errors + Stubbing vs. Spying + Eliminating Invalid Paths/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -0,0 +1,68 @@
+//
+//  RemoteFeedLoaderTest.swift
+//  RemoteFeedLoaderTest
+//
+//  Created by Kevin Peng on 2021-08-19.
+//
+
+import XCTest
+import EssentialFeed
+
+class RemoteFeedLoaderTest: XCTestCase {
+
+    func test_init_doesNotRequestDataFromURL() {
+        let (_, client) = makeSUT()
+        XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+
+    func test_load_requestsDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+
+        sut.load()
+
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
+
+    func test_loadTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+
+        sut.load()
+        sut.load()
+
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+
+        client.error = NSError(domain: "Test", code: 0)
+
+        var capturedErrors = [RemoteFeedLoader.Error]()
+
+        sut.load { capturedErrors.append($0) }
+
+        XCTAssertEqual(capturedErrors, [.connectivity])
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT(url: URL = URL(string: "https://a-given-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURLs = [URL]()
+        var error: Error?
+
+        func get(from url: URL, completion: @escaping (Error) -> Void) {
+            if let error = error {
+                completion(error)
+            }
+            requestedURLs.append(url)
+        }
+    }
+}
