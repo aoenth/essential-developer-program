95137141418fc0850354ae59a5abf4bb793abc5e
Kevin Peng <peng.kevin@me.com>
2022-01-25 08:48:03 -0500

    Lesson 11

---
+++ 11 - Automating Memory Leak Detection + Preventing a (Way Too Common) Async Bug/EssentialFeed/EssentialFeed/Feed Feature/FeedItem.swift
@@ -0,0 +1,22 @@
+//
+//  FeedItem.swift
+//  FeedItem
+//
+//  Created by Kevin Peng on 2021-08-19.
+//
+
+import Foundation
+
+public struct FeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
---
+++ 11 - Automating Memory Leak Detection + Preventing a (Way Too Common) Async Bug/EssentialFeed/EssentialFeed/Feed Feature/FeedLoader.swift
@@ -0,0 +1,19 @@
+//
+//  FeedLoader.swift
+//  FeedLoader
+//
+//  Created by Kevin Peng on 2021-08-21.
+//
+
+import Foundation
+
+public enum LoadFeedResult<Error: Swift.Error> {
+    case success([FeedItem])
+    case failure(Error)
+}
+
+protocol FeedLoader {
+    associatedtype Error: Swift.Error
+
+    func load(completion: @escaping (LoadFeedResult<Error>) -> Void)
+}
