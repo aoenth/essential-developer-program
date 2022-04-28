6f0e7943155768d5bca79264d8f9704b5430f441
Kevin Peng <peng.kevin@me.com>
2022-04-28 18:07:24 -0400

    Make URLProtocolStub intercept every network request instead of URL-based mocking since we don't want to ever perform a network request during tests. Also, we want our assertions to be more precise about the error when they fail.

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -13,7 +13,6 @@ class URLSessionHTTPClient {
 	}
 	
 	func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-        let url = URL(string: "http://wrong-url.com")!
 		session.dataTask(with: url) { _, _, error in
 			if let error = error {
 				completion(.failure(error))
@@ -28,7 +27,7 @@ class URLSessionHTTPClientTests: XCTestCase {
 		URLProtocolStub.startInterceptingRequests()
 		let url = URL(string: "http://any-url.com")!
 		let error = NSError(domain: "any error", code: 1)
-		URLProtocolStub.stub(url: url, data: nil, response: nil, error: error)
+		URLProtocolStub.stub(data: nil, response: nil, error: error)
 		
 		let sut = URLSessionHTTPClient()
 		
@@ -53,7 +52,7 @@ class URLSessionHTTPClientTests: XCTestCase {
 	// MARK: - Helpers
 	
 	private class URLProtocolStub: URLProtocol {
-		private static var stubs = [URL: Stub]()
+        private static var stub: Stub?
 		
 		private struct Stub {
 			let data: Data?
@@ -61,8 +60,8 @@ class URLSessionHTTPClientTests: XCTestCase {
 			let error: Error?
 		}
 		
-		static func stub(url: URL, data: Data?, response: URLResponse?, error: Error?) {
-			stubs[url] = Stub(data: data, response: response, error: error)
+		static func stub(data: Data?, response: URLResponse?, error: Error?) {
+			stub = Stub(data: data, response: response, error: error)
 		}
 		
 		static func startInterceptingRequests() {
@@ -71,31 +70,27 @@ class URLSessionHTTPClientTests: XCTestCase {
 		
 		static func stopInterceptingRequests() {
 			URLProtocol.unregisterClass(URLProtocolStub.self)
-			stubs = [:]
+			stub = nil
 		}
 		
 		override class func canInit(with request: URLRequest) -> Bool {
-			guard let url = request.url else { return false }
-			
-			return URLProtocolStub.stubs[url] != nil
-		}
+            return true
+        }
 		
 		override class func canonicalRequest(for request: URLRequest) -> URLRequest {
 			return request
 		}
 		
 		override func startLoading() {
-			guard let url = request.url, let stub = URLProtocolStub.stubs[url] else { return }
-			
-			if let data = stub.data {
+            if let data = Self.stub?.data {
 				client?.urlProtocol(self, didLoad: data)
 			}
 			
-			if let response = stub.response {
+			if let response = Self.stub?.response {
 				client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
 			}
 			
-			if let error = stub.error {
+			if let error = Self.stub?.error {
 				client?.urlProtocol(self, didFailWithError: error)
 			}
 			
