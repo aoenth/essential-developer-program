f8c8e3a85d2bf4bb361d7479f9b2947ed8ed8c87
Kevin Peng <pengkevi@amazon.com>
2022-06-27 09:14:30 -0400

    Move `FeedStore` to its own file

---
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,15 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Peng, Kevin [C] on 2022-06-27.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
